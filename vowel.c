53ba4c3b8335fc98ecda9216f27f8ab9ee5124c1
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-02-22 11:53:50 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char=c;
+printf("enter the letter");
+scanf("%c,&c");
+lowercase vowel='a','e','i','o','u';
+uppercase vowel='A','E','I','O','U';
+if(lowercase vowel||uppercase vowel)
+print("%c is vowel",c);
+else
+print("%c is consonant",c)
+}
