501782ddaf916f6d45934832b7b4f1d294601a78
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-02-22 12:20:02 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+printf("enter the letter");
+scanf("%c,&c");
+lowercase alp('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+uppercase alp('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+if(lowercase alp||uppercase alp)
+printf("%c is alphabet",c);
+else
+print("%c is number",c);
+}
