68ef73f201f5772b3053fad540a19fae3565eb2b
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-02-28 11:07:22 +0530

    Create palindrome1.py

---
+++ palindrome1.py
@@ -0,0 +1,14 @@
+def reverse(s):
+    return s[::-1]
+def ispalindrome(s)
+    rev=reverse(s)
+    if(s==rev):
+       return true
+    return false
+s="malayalam"
+ans=ispalindrome(s)
+if ans==1:
+   print("yes")
+else:
+   print("no")
+  
