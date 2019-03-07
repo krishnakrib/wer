ba2eaa1da45f50fa95a3ea82d9e2f6649caf7969
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-03-07 10:52:06 +0530

    Create disply prime.py

---
+++ disply prime.py
@@ -0,0 +1,10 @@
+a=20
+b=100
+print("number between",a,"and",b,"are")
+for num in range(a,b+1):
+    if (num>1):
+       for i in range(2,num):
+           if(num%i)==0:
+               break
+        else:
+           print(num)
