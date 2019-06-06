b712bbed1f7b68ab851e162e18830f585ba09fe0
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-06-06 15:30:48 +0530

    Create digits.py

---
+++ digits.py
@@ -0,0 +1,27 @@
+def countEvenOdd(n): 
+      
+    even_count = 0
+    odd_count = 0
+    while (n > 0): 
+        rem = n % 10
+        if (rem % 2 == 0): 
+            even_count += 1
+        else: 
+            odd_count += 1
+              
+        n = int(n / 10) 
+      
+    print( "Even count : " , even_count) 
+    print("\nOdd count : " , odd_count) 
+    if (even_count % 2 == 0 and
+                    odd_count % 2 != 0): 
+        return 1
+    else: 
+        return 0
+n = 2335453; 
+t = countEvenOdd(n); 
+  
+if (t == 1): 
+    print("YES") 
+else: 
+    print("NO") 
