76d69f1bbeead2681bbb46d7144e2626d4517135
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-07-10 16:36:16 +0530

    Create beginnar64.c

---
+++ beginnar64.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int apple,ball,cat;
+    scanf("%d",&apple);
+    scanf("%d",&ball);
+    cat=apple+ball;
+    if(cat%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
