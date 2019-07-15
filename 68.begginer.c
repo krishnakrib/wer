7b518a717ba60f5a856c8afafb8bd8629f13f9b0
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-07-15 10:04:06 +0530

    Create 68.begginer.c

---
+++ 68.begginer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+ {
+    int appple;
+    scanf("%d",&appple);
+    if(appple%7==0)
+    {
+        printf("yes",appple);
+    }
+    else
+    {
+        printf("no",appple);
+    }
+  return 0;
+}
